d41da1d71863816268e099a51e61c934cfbf3ed1
loti12345 <catargiuraul06@gmail.com>
2024-03-08 20:57:18 +0200

    rtlc

--- app/src/main/java/com/example/forbetter/rent_location.java
+++ app/src/main/java/com/example/forbetter/rent_location.java
@@ -1,6 +1,9 @@
 package com.example.forbetter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class rent_location extends AppCompatActivity {
+    public Button maps;
+    public void setMaps(){
+        maps = (Button) findViewById(R.id.ShowButton);
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent action = new Intent(rent_location.this,);
+                startActivity(action);
+            }
+        });
+    }
+    public Map
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
