d083a3d7b3285c160826c9c5f49e541d4f0999de
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-03-08 22:04:25 +0200

    Bianca ne obliga

--- app/src/main/java/com/example/forbetter/rent_location.java
+++ app/src/main/java/com/example/forbetter/rent_location.java
@@ -1,6 +1,11 @@
 package com.example.forbetter;
 
+import com.google.android.gms.maps.GoogleMap;
 import android.content.Intent;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,25 +18,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.maps.MapView;
 
-public class rent_location extends AppCompatActivity {
-    public Button maps;
-    public void setMaps(){
-        maps = (Button) findViewById(R.id.ShowButton);
-        maps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Intent action = new Intent(rent_location.this);
-//                startActivity(action);
-            }
-        });
-    }
-    public MapView mapView;
+public class rent_location extends AppCompatActivity implements onMapReadyCallback{
 
-    public void setMapView(MapView mapView) {
-        this.mapView = mapView;
-    }
 
-    public void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.rent_locations);
@@ -40,5 +30,13 @@ public class rent_location extends AppCompatActivity {
             v.setPadding(systemBars.left,systemBars.top,systemBars.right, systemBars.bottom);
             return insets;
         }));
+
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.map, mapFragment)
+                .commit();
+
+
     }
 }
