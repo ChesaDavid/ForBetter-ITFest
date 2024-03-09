9944f928463bb9e8b5f66bae7dd1b6b7dd0f7ed8
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-03-09 23:09:23 +0200

    split

--- app/src/main/java/com/example/forbetter/MainActivity.java
+++ app/src/main/java/com/example/forbetter/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -23,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        }));
+           }));
+        Button singUpBtn = findViewById(R.id.singupButton);
+//        if(singUpBtn.isEnabled()){
+//            singUpBtn.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent action = new Intent(MainActivity.this,singup.class);
+//                    startActivity(action);
+//                }
+//            });
+//        }
+    }
 
-    }}
+}
+
